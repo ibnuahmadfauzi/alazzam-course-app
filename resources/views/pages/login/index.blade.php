c38f1392eb07a3457170dda8170baf4cf12ee8fa
ibnuahmadfauzi <ibnu7897@gmail.com>
2023-08-02 19:51:58 +0700

    Membuat tampilan halaman login

---
+++ resources/views/pages/login/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.layout-1')
+
+@section('content')
+
+    <div class="row d-flex justify-content-center">
+        <div class="col-lg-5">
+            <div class="card mt-5">
+                <div class="card-body">
+                    <div class="d-flex justify-content-center my-2">
+                        <img src="/assets/img/logo.png" height="100" alt="Logo">
+                    </div>
+                    <div class="text-center">
+                        <h3 class="fw-bold">
+                            Sistem Akademik
+                            <br>
+                            Kursus Bahasa Inggris Al-Azzam
+                        </h3>
+                    </div>
+                    <hr>
+                    <div>
+                        <form>
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username :</label>
+                                <input type="text" id="username" name="username" placeholder="Masukkan username ..." class="form-control" autofocus required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password :</label>
+                                <input type="password" id="password" name="password" placeholder="Masukkan password ..." class="form-control" required>
+                            </div>
+                            <div class="row d-flex justify-content-center">
+                                <button type="submit" class="btn btn-outline-primary fw-bold col-lg-4"><i class="fa fa-sign-in" aria-hidden="true"></i> Masuk Akun</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="card-footer text-center text-secondary fw-bold">
+                    @include('partials.footer-text')
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/partials/footer-text.blade.php
@@ -0,0 +1,8 @@
+{{-- Footer Text --}}
+<p class="m-0 p-0">
+    Copyright &copy; {{ date("Y") }}
+</p>
+<p class="m-0 p-0">
+    Ibnu Ahmad Fauzi - Universitas Islam Balitar
+</p>
+{{-- end Footer Text --}}
